43753987a49d0ab3d39c57af32838ea9deceed97
natsuki <n_shimada0018@ftengi.co.jp>
2026-07-14 18:54:10 +0900

    Disable host authorization on Render

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -43,7 +43,7 @@ Rails.application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
-  config.hosts << "travel-money-manager.onrender.com"
+  config.host_authorization = { exclude: ->(_request) { true } }
 
   # Log to STDOUT by default
   config.logger = ActiveSupport::Logger.new(STDOUT)
